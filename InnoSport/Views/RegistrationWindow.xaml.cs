47ed7a3bcf9f8a17a8aa8a542f06ee91e4d6925d
Egor <dalinchikegor5@mail.ru>
2025-05-13 16:57:51 +0800

    StartDevelopment

--- InnoSport/Views/RegistrationWindow.xaml.cs
+++ InnoSport/Views/RegistrationWindow.xaml.cs
@@ -11,12 +11,9 @@ using System.Windows.Shapes;
 
 namespace InnoSport
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class RegistrationWindow : Window
     {
-        public MainWindow()
+        public RegistrationWindow()
         {
             InitializeComponent();
         }
