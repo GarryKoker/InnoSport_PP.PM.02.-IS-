b2f6ca720b4c7a55659d0140a20fbe818447bdab
Egor <dalinchikegor5@mail.ru>
2025-05-16 10:11:41 +0800

    Added ViewsCatalogs and 2 main windows

---
+++ InnoSport/Data/DBContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace InnoSport.Data
+{
+    public class AppDBContext: DbContext
+    {
+        public AppDBContext() => Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("postgresql://neondb_owner:npg_fOi73drXAFCm@ep-black-mud-a9oiu3p2-pooler.gwc.azure.neon.tech/neondb?sslmode=require");
+        }
+
+        public DbSet<InnoSport.Models.Users> Users { get; set; }
+    }
+}
---
+++ InnoSport/Models/Users.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace InnoSport.Models
+{
+    public class Users
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Login { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Otchestvo { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
